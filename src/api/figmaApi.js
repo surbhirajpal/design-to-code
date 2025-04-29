821597aac09ff4e4aa58ef30276f68507744cea9
surbhirajpal <surbhirajpal88@gmail.com>
2025-04-29 16:07:13 +0200

    design-to-code setup

--- src/api/figmaApi.js
+++ src/api/figmaApi.js
@@ -1,25 +1,23 @@
 import axios from "axios";
 
-//from figm account token
-const FIGMA_TOKEN = "figd_4tnfD-rqohXLIjcH_zc5zcn_VCHqvgeVExfMDypL";
+const figmaToken = import.meta.env.FIGMA_TOKEN;
+const figmaFileKey = import.meta.env.FILE_KEY;
+const nodeID = import.meta.env.NODE_ID;
 
-//from figma file url
-const FILE_KEY = "p8YCxiiqUdv8NNmhbHvW3t";
 
 // figma file url: https://www.figma.com/design/p8YCxiiqUdv8NNmhbHvW3t/Bootstrap-5-Design-System---UI-Kit--Community-?node-id=0-1&p=f&t=wZ1K0ACzf1K9m5uH-0
 // error from api: 400 - request too large
 // adding node id from url 'node-id=0-1'
-const NODE_ID = "0:1";
 
 export async function fetchFigmaNode() {
-  const url = `https://api.figma.com/v1/files/${FILE_KEY}/nodes?ids=${encodeURIComponent(
-    NODE_ID
+  const url = `https://api.figma.com/v1/files/${figmaFileKey}/nodes?ids=${encodeURIComponent(
+    nodeID
   )}`;
 
   try {
     const response = await axios.get(url, {
       headers: {
-        "X-Figma-Token": FIGMA_TOKEN,
+        "X-Figma-Token": figmaToken,
       },
     });
     return response.data;
